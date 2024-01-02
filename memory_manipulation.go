8d18c70cf6a6348ca45953d9b9d4da4d90d1ddc3
Bogdan Ovsiannikov <takahawkkun@gmail.com>
2024-01-02 20:54:50 +0200

    Implement STX, STY instructions

---
+++ memory_manipulation.go
@@ -0,0 +1,22 @@
+package emu6502
+
+// sta executes STA (STore Accumulator in memory). Argument is presumed to
+// be value stored already in the address, so that it is ignored
+func (cpu *CPU6502) sta(_ uint8) uint8 {
+	cpu.PC += 1
+	return cpu.A
+}
+
+// stx executes STX (STore X in memory). Argument is presumed to be value
+// stored already in the address, so that it is ignored
+func (cpu *CPU6502) stx(_ uint8) uint8 {
+	cpu.PC += 1
+	return cpu.X
+}
+
+// sty executes STY (STore Y in memory). Argument is presumed to be value
+// stored already in the address, so that it is ignored
+func (cpu *CPU6502) sty(_ uint8) uint8 {
+	cpu.PC += 1
+	return cpu.Y
+}
--- opcodes.go
+++ opcodes.go
@@ -410,10 +410,22 @@ var opcodeFunctions [0x100]func(cpu *CPU6502) = [0x100]func(cpu *CPU6502){
 	func(cpu *CPU6502) {
 		cpu.executeWithIndexedIndirect(cpu.sta)
 	},
+	// 0x82 is not defined, assign NOP function
 	nop,
+	// 0x83 is not defined, assign NOP function
 	nop,
-	nop,
-	nop,
+	// 0x84 STY, zero page
+	func(cpu *CPU6502) {
+		cpu.executeWithZeroPage(cpu.sty)
+	},
+	// 0x85 STA, zero page
+	func(cpu *CPU6502) {
+		cpu.executeWithZeroPage(cpu.sta)
+	},
+	// 0x86 STX, zero page
+	func(cpu *CPU6502) {
+		cpu.executeWithZeroPage(cpu.stx)
+	},
 	nop,
 	nop,
 	nop,
